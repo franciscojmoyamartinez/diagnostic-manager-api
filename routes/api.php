ad251d1c714d4c9626558f473e4ce63b33097d32
franciscojmoyamartinez <franciscojmoyamartinez@gmail.com>
2021-05-06 20:20:05 +0200

    PatientController - created

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PatientController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('patients', [PatientController::class, 'index']);
+Route::get('patients/{patient}', [PatientController::class, 'show']);
+Route::post('patients', [PatientController::class, 'store']);
+Route::put('patients/{patient}', [PatientController::class, 'update']);
+Route::delete('patients/{patient}', [PatientController::class, 'delete']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
