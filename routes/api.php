aed982d14c3c39b7454efc95e7cc008274fa51a6
franciscojmoyamartinez <franciscojmoyamartinez@gmail.com>
2021-05-06 20:35:07 +0200

    Register user controller - created

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\Auth\RegisterController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,7 +18,9 @@ Route::get('patients', [PatientController::class, 'index']);
 Route::get('patients/{patient}', [PatientController::class, 'show']);
 Route::post('patients', [PatientController::class, 'store']);
 Route::put('patients/{patient}', [PatientController::class, 'update']);
-Route::delete('patients/{patient}', [PatientController::class, 'delete']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::delete('patients/{patient}', [PatientController::class, 'destroy']);
+Route::post('register', [RegisterController::class, 'register']);
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
